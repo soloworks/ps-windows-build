41fb0b554dae0e716610f47fc6da97cb6a69e9ef
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-05-13 23:49:47 +0100

    Refined string creation, silenced Simpl+CC Install, Added Simple+ Install

--- Azure-AudioVisual-Tools-Agent.ps1
+++ Azure-AudioVisual-Tools-Agent.ps1
@@ -8,10 +8,11 @@
 # Currently supports:
 #
 #   - AMX Netlinx Studio 4.4.1626
+#   - Crestron Simpl+
 #   - Crestron Simpl+ Cross Compiler 1.3
 #
 # Todo:
-#   -  Find correct flags for S+CC silent install
+#   -  C:\Program Files (x86)\Crestron\Downloads\simpl_windows_4.11.06.01.exe /MASTERINSTALLER=TRUE /VERYSILENT /NORESTART /DIR="C:\Program Files (x86)\Crestron\Simpl" /LOG="C:\Program Files (x86)\Crestron\Downloads\InnoSetup.log" 
 #
 # Created by and for Solo Works London
 #
@@ -28,19 +29,34 @@ try{
     New-Item -Path $WorkDir -ItemType Directory -Force
 
     # AMX Netlinx Studio 4.4.1626 - NS.exe /help for options
-    $NetlinxVersion = '_4_4_1626'
-    Write-Output "AMX NetLinxStudio$NetlinxVersion Downloading"
-    Invoke-WebRequest -Uri "https://files.soloworks.co.uk/amx/NetLinxStudioSetup$NetlinxVersion.exe" -OutFile $WorkDir"NetLinxStudioSetup"$NetlinxVersion".exe"
+    $Version = '_4_4_1626'
+    $FileEXE = "$($WorkDir)NetLinxStudioSetup$($Version).exe"
+    Write-Output $FileEXE
+    Write-Output "AMX NetLinxStudio$($Version) Downloading"
+    Invoke-WebRequest -Uri "https://files.soloworks.co.uk/amx/NetLinxStudioSetup$($Version).exe" -OutFile $FileEXE
     Write-Output "AMX NetLinxStudio Installing"
-    Start-Process -FilePath $WorkDir"NetLinxStudioSetup"$NetlinxVersion".exe" -ArgumentList "/quiet" -Wait
+    Start-Process -FilePath $FileEXE -ArgumentList "/quiet" -Wait
     Write-Output "AMX NetlinxStudio Installed"
 
+    # Crestron SIMPL Windows
+    $Version = '_4.11.06.01'
+    $FileEXE = "$($WorkDir)simpl_windows$($Version).exe"
+    Write-Output "Crestron SimplWindows$($Version) Downloading"
+    Invoke-WebRequest -Uri "https://files.soloworks.co.uk/crestron/simpl_windows$($Version).exe" -OutFile $FileEXE
+    Write-Output "Crestron Simpl Windows Installing"
+    Start-Process -FilePath $FileEXE -ArgumentList "/quiet" -Wait
+    Write-Output "Crestron Simpl Windows Installed"
+
     # Crestron Simpl+ Cross Compiler 1.3
-    $CrossCompilerVersion = '_1.3'
-    Write-Output "Crestron Simpl+CC$CrossCompilerVersion Downloading"
-    Invoke-WebRequest -Uri "https://files.soloworks.co.uk/crestron/simpl_plus_cross_compiler$CrossCompilerVersion.exe" -OutFile $WorkDir"simpl_plus_cross_compiler"$CrossCompilerVersion".exe"
+    $Version = '_1.3'
+    $FileEXE = "$($WorkDir)simpl_plus_cross_compiler$($Version).exe"
+    $FileISS = "$($WorkDir)simpl_plus_cross_compiler$($Version).iss"
+    Write-Output "Crestron Simpl+CC$($Version) Downloading"
+    Invoke-WebRequest -Uri "https://files.soloworks.co.uk/crestron/simpl_plus_cross_compiler$($Version).exe" -OutFile $FileEXE
+    Invoke-WebRequest -Uri "https://files.soloworks.co.uk/crestron/simpl_plus_cross_compiler$($Version).iss" -OutFile $FileISS
+    
     Write-Output "Crestron Simpl+CC Installing"
-    Start-Process -FilePath $WorkDir"simpl_plus_cross_compiler$CrossCompilerVersion.exe" -Wait #-ArgumentList "/S /v`"qn`""
+    Start-Process -FilePath $FileEXE -ArgumentList "/a /s /f1`"$($FileISS)`"" -Wait
     Write-Output "Crestron Simpl+CC Installed"
 
 }
